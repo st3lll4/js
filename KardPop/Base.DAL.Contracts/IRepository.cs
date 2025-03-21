da8150033186294654988d74c6febcd78b4c1b18
Stella Tukia <148345190+sttuki@users.noreply.github.com>
2025-03-21 11:08:02 +0200

    separate kardpop and js

---
+++ KardPop/Base.DAL.Contracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Base.DAL.Contracts;
+using Base.Domain;
+
+public interface IRepository<TEntity> 
+where TEntity : BaseEntity
+    {
+        IEnumerable<TEntity> All();
+
+        TEntity Find(params object[] id);
+        Task<TEntity> FindAsync(params object[] id);
+
+        void Add(TEntity entity);
+        Task AddAsync(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        void Remove(TEntity entity);
+
+        void Remove(params object[] id);
+    }
+    
