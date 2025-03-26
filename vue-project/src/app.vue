30309246642cfacc050851a7f3d3e0079e73dc4c
Stella Tukia <148345190+sttuki@users.noreply.github.com>
2025-03-26 16:22:56 +0200

    cmt

--- vue-project/src/app.vue
+++ vue-project/src/app.vue
@@ -1,12 +1,13 @@
 <script setup>
 import { ref, computed, reactive } from 'vue';
 import Nav from './components/nav.vue'
-
+import './assets/style.css'
 
 </script>
 
 <template>
   <Nav/>
+  OIIIIII
 </template>
 
 <style scoped></style>
--- vue-project/src/components/nav.vue
+++ vue-project/src/components/nav.vue
@@ -5,22 +5,14 @@ import '../assets/style.css'
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+  <nav>
     <div class="container">
-      <a class="navbar-brand" href="#">Fixed navbar</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+        <ul>
           <li class="nav-item">
             <RouterLink class="nav-link" to="/">Home</RouterLink>
           </li>
-          
         </ul>
       </div>
-    </div>
   </nav>
 </template>
 
@@ -29,40 +21,23 @@ import '../assets/style.css'
 nav {
   background: linear-gradient(90deg, rgb(255, 255, 255) 0%, rgb(58, 116, 217) 100%);
   font-size: 1.15rem;
+  height: 3rem;
+  display: flex;
+  align-items: center;
 }
 
-.navigation-link {
+ul {
+  list-style: none;
+}
+
+.nav-item {
   color: #214788; 
   padding-left: 1rem;
   padding-right: 1rem;
   text-decoration: none;
 }
-.navigation-link:hover {
+.nav-item:hover {
   color: #3A74D9;
 }
-
-.icon {
-  background-color: white;
-  color: black;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 0.5rem;
-  margin: 0 2vh 0 2vh;
-  transition: 0.3s;
-}
-.icon:hover {
-  background-color: #3A74D9;
-  color: white;
-  border: 1px white;
-}
-
-nav .container {
-  max-width: 1400px;
-  margin: 0 auto;
-  width: 100%;
-}
-
 
 </style>
