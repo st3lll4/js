a4ce24a379296e181ff9e40c03a3cfe2e340fe1a
Stella Tukia <sttuki@taltech.ee>
2025-04-13 16:26:39 +0300

    deploy

--- vue-project/src/router/index.ts
+++ vue-project/src/router/index.ts
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Game from "../views/Game.vue";
 import Beginning from "../views/Beginning.vue";
 
-const base = '/js/vue/';
+const base = '/js/vue-project/';
 
 const routes = [
     {
--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/js/vue/',
+  base: '/js/vue-project/',
   plugins: [
     vue(),
     vueDevTools(),
