576dec309ed16d999922ec16a53d8b4d79a8ce9f
Stella Tukia <sttuki@taltech.ee>
2025-04-13 16:28:58 +0300

    deploy attempt 5

--- vue-project/vite.config.js
+++ vue-project/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
-  base: '/js/vue-project/',
+  base: '/js/vue-project/dist/',
   plugins: [
     vue(),
     vueDevTools(),
